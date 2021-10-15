02ab3f252286ffe6c96abd72eae2d5e64582cbb5
Gmagnel Talang Emmanuel <83882744+Gmagnel@users.noreply.github.com>
2021-10-15 12:33:00 +0000

    Setting up app for basic node and express with free code camp

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
-const { Router } = require('express');
+
+var bGround = require('fcc-express-bground');
 var express = require('express');
-const { setupBackgroundApp } = require('fcc-express-bground');
+
 var app = express();
 
-console.log("Hello World");
+bGround.log("Hello World");
 
 
 
